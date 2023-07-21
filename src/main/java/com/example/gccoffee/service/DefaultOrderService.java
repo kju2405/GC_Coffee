62b9d102b1443d86fc4c33c2881b845e86339045
jaeungKim <kju2405@gmail.com>
2023-07-21 16:50:16 +0900

    feat: orderService 구현

---
+++ src/main/java/com/example/gccoffee/service/DefaultOrderService.java
@@ -0,0 +1,50 @@
+package com.example.gccoffee.service;
+
+import com.example.gccoffee.model.Email;
+import com.example.gccoffee.model.Order;
+import com.example.gccoffee.model.OrderItem;
+import com.example.gccoffee.model.OrderStatus;
+import com.example.gccoffee.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class DefaultOrderService implements OrderService{
+    private final OrderRepository orderRepository;
+
+    public DefaultOrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @Override
+    public Order createOrder(Email email, String address, String postcode, List<OrderItem> orderItems) {
+        Order order = new Order(
+            UUID.randomUUID().toString(),
+            email,
+            address,
+            postcode,
+            orderItems,
+            OrderStatus.ACCEPTED,
+            LocalDateTime.now(),
+            LocalDateTime.now());
+        return orderRepository.insert(order);
+    }
+
+    @Override
+    public List<Order> findAllOrders() {
+        return orderRepository.findAllOrders();
+    }
+
+    @Override
+    public Order findOrdersById(String orderId) {
+        return orderRepository.findOrdersById(orderId);
+    }
+
+    @Override
+    public Order updateOrder(Order order) {
+        return orderRepository.update(order);
+    }
+}
---
+++ src/main/java/com/example/gccoffee/service/OrderService.java
@@ -0,0 +1,17 @@
+package com.example.gccoffee.service;
+
+import com.example.gccoffee.model.Email;
+import com.example.gccoffee.model.Order;
+import com.example.gccoffee.model.OrderItem;
+
+import java.util.List;
+
+public interface OrderService {
+    Order createOrder(Email email, String address, String postcode, List<OrderItem> orderItems);
+
+    List<Order> findAllOrders();
+
+    Order findOrdersById(String orderId);
+
+    Order updateOrder(Order order);
+}
